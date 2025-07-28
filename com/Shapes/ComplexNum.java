ccc5608c6a2eca7b8e79563d95fc079fe87aa78a
Heorhii Kozachok <goshakozachok@gmail.com>
2025-07-28 19:15:04 +0300

    calc module

--- com/Shapes/ComplexNum.java
+++ com/Shapes/ComplexNum.java
@@ -2,25 +2,42 @@ package com.Shapes;
 
 public class ComplexNum{
    private double rNum;
-   private double eNum;
+   private double iNum;
 
-   public ComplexNum(double rNum, double eNum) {
+   public ComplexNum(double rNum, double iNum) {
       this.rNum = rNum;
-      this.eNum = eNum;
+      this.iNum = iNum;
    }
    public double absN() {
-      return Math.sqrt(eNum*eNum+rNum*rNum);
+      return Math.sqrt(iNum*iNum+rNum*rNum);
    }
+   public ComplexNum getReciprocal(){
+      double denom = iNum*iNum+rNum*rNum;
+      if (denom !=0){
+         return new ComplexNum(rNum/denom, -iNum/denom);
+      } else{
+         throw new IllegalStateException("z=0");
+      }
+   }
+
    public double getrNum() {
       return rNum;
    }
    public void setrNum(double rNum) {
       this.rNum = rNum;
    }
-   public double geteNum() {
-      return eNum;
+   public double getiNum() {
+      return iNum;
    }
-   public void seteNum(double eNum) {
-      this.eNum = eNum;
+   public void setiNum(double iNum) {
+      this.iNum = iNum;
+   }
+
+   @java.lang.Override
+   public java.lang.String toString() {
+      return "ComplexNum{" +
+              "reNum=" + rNum +
+              ", imNum=" + iNum +
+              '}';
    }
 }
--- com/calculators/Calculator.java
+++ com/calculators/Calculator.java
@@ -5,8 +5,10 @@ import com.Shapes.ComplexNum;
 public class Calculator{
     public static void main(String[] args) {
         ComplexNum  CN = new ComplexNum(4, 4);
-        double v= CN.absN;
+        double v= CN.absN();
+        ComplexNum h= CN.getReciprocal();
         System.out.println(v);
+        System.out.println(h);
     }
 }
 
