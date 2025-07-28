c0f571c1f246f568f06535093e436bc56cfb8dc7
Heorhii Kozachok <goshakozachok@gmail.com>
2025-07-28 18:16:54 +0300

    some files

--- .idea/com/Shapes/ComplexNum.java
+++ .idea/com/Shapes/ComplexNum.java
@@ -1,2 +1,18 @@
 package com.Shapes;
 
+public class ComplexNum{
+   private double rNum;
+   private double eNum;
+   public double getrNum() {
+      return rNum;
+   }
+   public void setrNum(double rNum) {
+      this.rNum = rNum;
+   }
+   public double geteNum() {
+      return eNum;
+   }
+   public void seteNum(double eNum) {
+      this.eNum = eNum;
+   }
+}
